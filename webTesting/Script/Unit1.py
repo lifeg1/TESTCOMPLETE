e0613e603794eb2b45a8c3db6a9638afc84275d9
lifeg1 <jeevan.sharma160@gmail.com>
2023-04-04 14:03:43 +0530

    new update

--- webTesting/Script/Unit1.py
+++ webTesting/Script/Unit1.py
@@ -1,1 +1,12 @@
-﻿
+﻿def Test1():
+  Browsers.Item[btChrome].Navigate("https://confluence.atlassian.com/bitbucketserver/basic-git-commands-776639767.html")
+  browser = Aliases.browser
+  page=browser.Page("*") #expecting to be this page *-  any page
+  #links=page.NativeWebObject.Find()
+  
+  links=page.contentDocument.links
+  for i in range(links.length):
+    Log.Message(links.item(i).InnerHtml)
+  
+  
+  
